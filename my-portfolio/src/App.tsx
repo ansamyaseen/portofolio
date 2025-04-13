58fad04a3af92d4ef0cf54535b319b456a007e95
Ansam Yaseen <ansamyaseen80@gmail.com>
2025-04-13 12:21:30 +0200

    01

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -13,9 +12,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     
       </div>
       <div className="container mt-5">
       <h1 className="text-primary">Hello, Bootstrap + TypeScript!</h1>
